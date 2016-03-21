c9d5aa735002f541dae843c5325fa5384823c812
Dmitry Kurkin <kurkindmit@gmail.com>
2016-03-22 00:32:34 +0300

    defence01 cat

--- cat/cat.c
+++ cat/cat.c
@@ -1,13 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/uio.h>
 #include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
 
-int main() {
+int main(int argv, char** args) {
+
+    int fd = 0;
+
+    if (argv == 2) {
+        fd = open(args[1], O_RDONLY);
+        if (fd == -1) {
+            perror("очень жаль");
+            return -1;
+        }       
+    } 
+
     while (!0) {
         size_t buff_size = 5;
         char in[buff_size];
-        size_t nread = read(0, &in[0], buff_size);
+        size_t nread = read(fd, &in[0], buff_size);
+        if (nread == -1 && errno == EINTR) {
+            printf("EINTR\n");
+            continue;
+        }
+        if (nread == -1) {
+            perror("очень жаль");
+            return -1;
+        }
+ 
         if (nread <= 0) {
             break;
         } else {
@@ -17,4 +39,7 @@ int main() {
             }
         }
     }
+
+    if (fd)
+        close(fd);
 }
